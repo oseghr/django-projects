fcdbfcdbef3e2cef01e0f73fbe2594c19f1a3379
Ose Benson <o.oaikhena@yahoo.com>
2022-06-19 19:39:23 -0600

    Update the Post models

--- .env/I4G031075DBC/blog/models.py
+++ .env/I4G031075DBC/blog/models.py
@@ -1,6 +1,18 @@
 from pyexpat import model
+from django.conf import settings
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField(max_length=1000)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_date = models.DateTimeField('created_date')
+    published_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
 
 
