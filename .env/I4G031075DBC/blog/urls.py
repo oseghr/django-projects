eb0da0057f10b4f5381690d3a57084934ff60f28
Ose Benson <o.oaikhena@yahoo.com>
2022-06-24 00:06:41 -0600

    update models, add templates and views

--- .env/I4G031075DBC/blog/urls.py
+++ .env/I4G031075DBC/blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path("create/", views.PostCreateView, name="PostCreateView"),
+    path("delete/", views.PostDeleteView, name="PostDeleteView"),
+    path("update/", views.PostUpdateView, name="PostUpdateView"),
+    path("read/", views.PostListView, name="PostListView"),
 ]
--- .env/I4G031075DBC/blog/views.py
+++ .env/I4G031075DBC/blog/views.py
@@ -1,9 +1,43 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
-from django.http import HttpResponse
-
 
 def home(request):
-    return HttpResponse("<h1>Hello, People. Welcome to my Blog</h1>")
+    post_list = Post.objects.all()
+    context = { "post_list": post_list }
+    return render(request, "blog/index.html", context)
+
+def PostListView(request):
+    post_list = Post.objects.all()
+    context = { "post_list": post_list }
+    return render(request, "blog/post-list.html", context)
+
+def PostCreateView(request):
+    title = request.POST.get("title") 		
+    body = request.POST.get("body") 
+
+    if request.method == "POST":
+        post = Post.objects.create(title=title, body=body)
+        post.save()
+    context = { }
+    return render(request, "blog/post-form.html", context)
+
+
+
+def PostDetailView(request):
+
+    context = {}
+    return render(request, "blog/post-detail.html", context)
+
+def PostUpdateView(request):
+    posts = Post.objects.all()
+    context = { }
+    return render(request, "blog/post-confirm-delete.html", context)
+
+def PostDeleteView(request):
+    posts = Post.objects.all()
+    context = { "posts": posts }
+    return render(request, "blog/post-confirm-delete.html", context)
